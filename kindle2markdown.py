59a6a91842d87f7e64386d1b592598483836b761
jiongmumu <lxia617@163.com>
2017-08-01 16:12:17 -0700

    Update kindle2markdown.py

--- kindle2markdown.py
+++ kindle2markdown.py
@@ -11,7 +11,7 @@ with open('./book.csv') as csvfile:
       bookname = row[0]
     if find and len(row) == 4:
       pages = [int(s) for s in row[1].split() if s.isdigit()]
-      result = result + '#Page'+ str(pages[0]) + '\n>'+ row[3] +  '\n\n评论：\n'
+      result = result + '#### Page'+ str(pages[0]) + '\n>'+ row[3] +  '\n\nComment：\n'
     if len(row) > 0 and row[0].find('批注类型') != -1:
       find = True
 
