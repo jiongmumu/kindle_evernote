0050dbe457769b73adff8a34b24d2d67020d5735
jiongmumu <lxia617@163.com>
2017-08-01 15:57:38 -0700

    Update kindle2markdown.py

--- kindle2markdown.py
+++ kindle2markdown.py
@@ -6,6 +6,6 @@ with open('./book.csv') as csvfile:
   for row in spamreader:
     if find and len(row) == 4:
       pages = [int(s) for s in row[1].split() if s.isdigit()]
-      print('#Page', pages[0], '\n>', row[3], '\n\n评论：\n')
+      print('#### Page', pages[0], '\n>', row[3], '\n\nComment：\n')
     if len(row) > 0 and row[0].find('批注类型') != -1:
       find = True
