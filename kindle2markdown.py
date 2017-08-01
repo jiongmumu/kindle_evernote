838396d3f61fd38f9e3cd307090cf1bd7c1467b5
jiongmumu <lxia617@163.com>
2017-08-01 16:08:04 -0700

    save to file according to bookname

--- kindle2markdown.py
+++ kindle2markdown.py
@@ -3,9 +3,17 @@ with open('./book.csv') as csvfile:
   spamreader = csv.reader(csvfile, delimiter=',')
   i = 0
   find = False
+  bookname = ''
+  result = ''
   for row in spamreader:
+    i = i + 1
+    if i == 2:
+      bookname = row[0]
     if find and len(row) == 4:
       pages = [int(s) for s in row[1].split() if s.isdigit()]
-      print('#### Page', pages[0], '\n>', row[3], '\n\nComment：\n')
+      result = result + '#Page'+ str(pages[0]) + '\n>'+ row[3] +  '\n\n评论：\n'
     if len(row) > 0 and row[0].find('批注类型') != -1:
       find = True
+
+with open(bookname + '.md', 'w') as f:
+  f.write(result)
