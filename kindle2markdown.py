1b009054ecbabfcd8d5e62a5191e35df6b47cab6
jiongmumu <lxia617@163.com>
2017-08-01 15:52:10 -0700

    Update kindle2markdown.py

--- kindle2markdown.py
+++ kindle2markdown.py
@@ -1,19 +1,11 @@
-"""TODO(lixx): DO NOT SUBMIT without one-line documentation for kindle2markdown.
-
-TODO(lixx): DO NOT SUBMIT without a detailed description of kindle2markdown.
-"""
-
 import csv
 with open('./book.csv') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',')
-      i = 0
-        find = False
-          for row in spamreader:
-                if find and len(row) == 4:
-                        pages = [int(s) for s in row[1].split() if s.isdigit()]
-                              print('#Page', pages[0], '\n>', row[3], '\n\n评论：\n')
-                                  if len(row) > 0 and row[0].find('批注类型') != -1:
-                                          find = True
-
-
-
+  spamreader = csv.reader(csvfile, delimiter=',')
+  i = 0
+  find = False
+  for row in spamreader:
+    if find and len(row) == 4:
+      pages = [int(s) for s in row[1].split() if s.isdigit()]
+      print('#Page', pages[0], '\n>', row[3], '\n\n评论：\n')
+    if len(row) > 0 and row[0].find('批注类型') != -1:
+      find = True
